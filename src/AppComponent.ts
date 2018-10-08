4ecbab5b9a2192dbde710013367a58139f0b09c9
Davis Sylvester <davis@sylvesterllc.com>
2018-10-07 23:21:59 -0500

    Setting project up to work with Typescript

---
+++ src/AppComponent.ts
@@ -0,0 +1,18 @@
+import Vue from "vue"
+import Component from "vue-class-component"
+
+
+@Component({
+  
+  template: "<button @click=\"onClick\">Click!</button>"
+})
+
+export default class MyComponent extends Vue {
+  
+  message: string = "Hello!"
+
+  
+  onClick (): void {
+    window.alert(this.message)
+  }
+}
